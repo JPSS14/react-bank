d614bf9b7a3fb938159e55ff3a076b7be5bb41a6
JPSS14 <jp_scarabelli@hotmail.com>
2021-04-07 18:38:03 -0300

    react-bank UPDATE 7: Added style to ProductsBox articles

--- src/components/ProductsBox.tsx
+++ src/components/ProductsBox.tsx
@@ -7,26 +7,30 @@ export function ProductsBox() {
                 <h1>Produtos</h1>
             </header>
 
-            <article>
+            <article className={style.mainProductsArticle}>
                 <header>
+                    <img src="/smartphones-main-products.jpg" alt="Smartphones" title="Smartphones" />
                     <h2>Smartphones</h2>
                 </header>
             </article>
 
-            <article>
+            <article className={style.mainProductsArticle}>
                 <header>
+                    <img src="/cars-main-products.jpg" alt="Carros" title="Carros" />
                     <h2>Carros</h2>
                 </header>
             </article>
 
-            <article>
+            <article className={style.mainProductsArticle}>
                 <header>
+                    <img src="/houses-main-products.jpg" alt="Casas" title="Casas" />
                     <h2>Casas</h2>
                 </header>
             </article>
 
-            <article>
+            <article className={style.mainProductsArticle}>
                 <header>
+                    <img src="/travel-main-products.jpg" alt="Viagens" title="Viagens" />
                     <h2>Viagens</h2>
                 </header>
             </article>
