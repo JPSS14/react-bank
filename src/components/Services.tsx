e488d658da6a3b87e743327e077ee0e52940bfd4
JPSS14 <jp_scarabelli@hotmail.com>
2021-04-09 21:08:46 -0300

    react-bank UPDATE 16: Added link to services component and some styles

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,26 +1,41 @@
 import style from '../styles/Services.module.scss';
+import Link from 'next/link';
 
 export function Services() {
     return (
         <>
-            <div className={style.mainPix}>
-                <h1>Pix</h1>
-            </div>
-            <div className={style.mainPix}>
-                <h1>Transferência</h1>
-            </div>
-            <div className={style.mainPix}>
-                <h1>Inserir Saldo</h1>
-            </div>
-            <div className={style.mainPix}>
-                <h1>Empréstimo</h1>
-            </div>
-            <div className={style.mainPix}>
-                <h1>Crédito</h1>
-            </div>
-            <div className={style.mainPix}>
-                <h1>Dívidas</h1>
-            </div>
+            <Link href="pix">
+                <a className={style.mainService}>
+                    <div >
+                        <h1>Pix</h1>
+                    </div>
+                </a>
+            </Link>
+            <Link href="transferencia">
+                <div className={style.mainService}>
+                    <h1>Transferência</h1>
+                </div>
+            </Link>
+            <Link href="inserir-saldo">
+                <div className={style.mainService}>
+                    <h1>Inserir Saldo</h1>
+                </div>
+            </Link>
+            <Link href="emprestimo">
+                <div className={style.mainService}>
+                    <h1>Empréstimo</h1>
+                </div>
+            </Link>
+            <Link href="credito">
+                <div className={style.mainService}>
+                    <h1>Crédito</h1>
+                </div>
+            </Link>
+            <Link href="dividas">
+                <div className={style.mainService}>
+                    <h1>Dívidas</h1>
+                </div>
+            </Link>
         </>
     );
 }
