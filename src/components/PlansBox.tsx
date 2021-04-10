4b3efa47a1f7c0a6fc753e094405f5dfc332a022
JPSS14 <jp_scarabelli@hotmail.com>
2021-04-09 22:58:38 -0300

    react-bank UPDATE 17: Added interation style to the plans component and fixed services component sintax

--- src/components/PlansBox.tsx
+++ src/components/PlansBox.tsx
@@ -1,48 +1,73 @@
 import style from '../styles/PlansBox.module.scss';
+import Link from 'next/link';
 
 export function PlansBox() {
     return (
         <>
-        <div className={style.mainPlansBox}>
-            <div className={style.mainPlansHeader}>
-                <header>
-                    <h1>Planos</h1>
-                </header>
-            </div>
-            <div className={style.content}>
-                <section className={style.mainPlansSection}>
-                    <article className={style.mainPlansHelp}>
-                        <header>
-                            <h2>?</h2>
-                        </header>
-                    </article>
-                    <article className={style.mainPlansArticle}>
-                        <header>
-                            <h2>Prata</h2>
-                        </header>
-                    </article>
-                    <article className={style.mainPlansArticle}>
-                        <header>
-                            <h2>Ouro</h2>
-                        </header>
-                    </article>
-                    <article className={style.mainPlansArticle}>
-                        <header>
-                            <h2>Platina</h2>
-                        </header>
-                    </article>
-                </section>
-            </div>
-        </div>
+            <div className={style.mainPlansBox}>
+                <div className={style.mainPlansHeader}>
+                    <header>
+                        <h1>Planos</h1>
+                    </header>
+                </div>
+                <div className={style.content}>
+                    <section className={style.mainPlansSection}>
 
+                        <Link href="/planos">
+                            <a className={style.mainPlansHelp}>
+                                <article>
+                                    <header>
+                                        <h2>?</h2>
+                                    </header>
+                                </article>
+                            </a>
+                        </Link>
 
-        <div className={style.mainPlansBoxMobile}>
-            <div className={style.mainPlansHeader}>
-                <header>
-                    <h1>Planos</h1>
-                </header>
+                        <Link href="/prata">
+                            <a className={style.mainPlansArticle}>
+                                <article>
+                                    <header>
+                                        <h2>Prata</h2>
+                                    </header>
+                                </article>
+                            </a>
+                        </Link>
+
+                        <Link href="/ouro">
+                            <a className={style.mainPlansArticle}>
+                                <article>
+                                    <header>
+                                        <h2>Ouro</h2>
+                                    </header>
+                                </article>
+                            </a>
+                        </Link>
+
+                        <Link href="/platina">
+                            <a className={style.mainPlansArticle}>
+                                <article>
+                                    <header>
+                                        <h2>Platina</h2>
+                                    </header>
+                                </article>
+                            </a>
+                        </Link>
+
+                    </section>
+                </div>
             </div>
-        </div>
+
+            <Link href="/planos">
+                <a  className={style.mainPlansBoxMobile}>
+                    <div>
+                        <div>
+                            <header className={style.mainPlansHeader}>
+                                <h1>Planos</h1>
+                            </header>
+                        </div>
+                    </div>
+                </a>
+            </Link>
         </>
     );
 }
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,37 +4,47 @@ import Link from 'next/link';
 export function Services() {
     return (
         <>
-            <Link href="pix">
+            <Link href="/pix">
                 <a className={style.mainService}>
-                    <div >
+                    <div>
                         <h1>Pix</h1>
                     </div>
                 </a>
             </Link>
-            <Link href="transferencia">
-                <div className={style.mainService}>
-                    <h1>Transferência</h1>
-                </div>
+            <Link href="/transferencia">
+                <a className={style.mainService}>
+                    <div>
+                        <h1>Transferência</h1>
+                    </div>
+                </a>
             </Link>
-            <Link href="inserir-saldo">
-                <div className={style.mainService}>
-                    <h1>Inserir Saldo</h1>
-                </div>
+            <Link href="/inserir-saldo">
+                <a className={style.mainService}>
+                    <div>
+                        <h1>Inserir Saldo</h1>
+                    </div>
+                </a>
             </Link>
-            <Link href="emprestimo">
-                <div className={style.mainService}>
-                    <h1>Empréstimo</h1>
-                </div>
+            <Link href="/emprestimo">
+                <a className={style.mainService}>
+                    <div>
+                        <h1>Empréstimo</h1>
+                    </div>
+                </a>
             </Link>
-            <Link href="credito">
-                <div className={style.mainService}>
-                    <h1>Crédito</h1>
-                </div>
+            <Link href="/credito">
+                <a className={style.mainService}>
+                    <div>
+                        <h1>Crédito</h1>
+                    </div>
+                </a>
             </Link>
-            <Link href="dividas">
-                <div className={style.mainService}>
-                    <h1>Dívidas</h1>
-                </div>
+            <Link href="/dividas">
+                <a className={style.mainService}>
+                    <div>
+                        <h1>Dívidas</h1>
+                    </div>
+                </a>
             </Link>
         </>
     );
