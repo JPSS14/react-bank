c42725927469c90f30a43151cf2533865d253821
JPSS14 <jp_scarabelli@hotmail.com>
2021-04-11 22:52:27 -0300

    react-bank UPDATE 24: Added style to login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,19 @@
 import Head from 'next/head'
 import { Header } from '../components/Header';
-import { Balance } from '../components/Balance';
-import { Profile } from '../components/Profile';
 import style from '../styles/main.module.scss';
-import { Services } from '../components/Services';
-import { PlansBox } from '../components/PlansBox';
-import { ProductsBox } from '../components/ProductsBox';
 import { Footer } from '../components/Footer';
 import { useState } from 'react';
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const router = useRouter();
 
-  function login(){
-    if((name != "") && (password != "")){
+  function login() {
+    if ((name != "") && (password != "")) {
       router.push("/conta");
-      console.log("nome ",name,"senha: ", password);
+      console.log("nome ", name, "senha: ", password);
     }
   }
 
@@ -33,13 +28,11 @@ export default function Home() {
           <header>
             <h1>Login</h1>
           </header>
-          <div>
+          <div className={style.loginBox}>
             <p>Cadastre-se no React Bank!</p>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-
-                <button onClick={login}>Entrar</button>
-
+            <button onClick={login}>Entrar</button>
           </div>
         </div>
       </main>
