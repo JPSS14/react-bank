302571c1a2fa198edd69f2f8552ca9fff0cbddb0
JPSS14 <jp_scarabelli@hotmail.com>
2021-04-22 00:34:13 -0300

    react-bank UPDATE 43: Deleted Friend component

--- src/pages/lista-de-amigos.tsx
+++ src/pages/lista-de-amigos.tsx
@@ -1,8 +1,20 @@
 import style from '../styles/main.module.scss';
+import { useContext, useEffect, useState } from 'react';
 import friendStyle from '../styles/listaAmigos.module.scss';
-import {Friend} from '../components/Friend';
+import friends from '../../friends.json';
+import { PixCard } from '../components/PixCard';
 
 export default function ListaDeAmigos() {
+    const [friendList, setFriendList] = useState([]);
+
+    useEffect(() => {
+        const loadAll = async () => {
+            let list = friends;
+            setFriendList(list);
+        }
+        loadAll();
+    }, []);
+
     return (
         <main className={style.main}>
             <section className={friendStyle.mainFriend}>
@@ -10,7 +22,11 @@ export default function ListaDeAmigos() {
                     <h1>Lista de Amigos</h1>
                 </header>
 
-                <Friend/>
+                {friendList.map((item, key) => (
+                    <>
+                        <PixCard key={key} img={item.img} nome={item.nome} optionCelular={item.optionCelular} optionCpf={item.optionCpf} optionEmail={item.optionEmail} />
+                    </>
+                ))}
 
             </section>
         </main>
