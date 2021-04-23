f6940facdd422f68d637459cd8d96b3b47d63808
JPSS14 <jp_scarabelli@hotmail.com>
2021-04-22 21:49:30 -0300

    react-bank UPDATE 48: Added style to planos page

--- src/pages/planos.tsx
+++ src/pages/planos.tsx
@@ -1,60 +1,67 @@
+import planStyle from '../styles/Planos.module.scss';
 import style from '../styles/main.module.scss';
 
-export default function Planos(){
-    return(
+export default function Planos() {
+    return (
         <main className={style.main}>
-            <section>
+            <section className={planStyle.mainSection}>
                 <header>
                     <h1>Planos</h1>
                 </header>
 
-                <article>
+                <article className={`${planStyle.mainArticle} ${planStyle.silver}`}>
                     <header>
                         <h2>Prata</h2>
                     </header>
-                    <h3>Requisito</h3>
-                    <p>Para ser um membro do plano de prata você precisa ter no mínimo R$ 1000,00.</p>
-                    <h3>Crédito</h3>
-                    <p>Com o plano prata você tem direito a pedir R$ 1000,00 de crédito!</p>
-                    <h3>Empréstimo</h3>
-                    <p>Com o plano prata você pode pedir até R$ 1000,00 de emprestimo, com juros de 20%.</p>
-                    <h3>Saldo Especial</h3>
-                    <p>Com o plano prata você pode ficar até com R$ -1000,00.</p>
+                    <div className={planStyle.mainArticleContent}>
+                        <h3>Requisito</h3>
+                        <p>Para ser um membro do plano de prata você precisa ter no mínimo R$ 1000,00.</p>
+                        <h3>Crédito</h3>
+                        <p>Com o plano prata você tem direito a pedir R$ 1000,00 de crédito!</p>
+                        <h3>Empréstimo</h3>
+                        <p>Com o plano prata você pode pedir até R$ 1000,00 de emprestimo, com juros de 20%.</p>
+                        <h3>Saldo Especial</h3>
+                        <p>Com o plano prata você pode ficar até com R$ -1000,00.</p>
+                    </div>
                     <button>Selecionar</button>
                 </article>
 
-                <article>
+                <article className={`${planStyle.mainArticle} ${planStyle.gold}`}>
                     <header>
                         <h2>Ouro</h2>
                     </header>
-                    <h3>Requisito</h3>
-                    <p>Para ser um membro do plano de ouro você precisa ter no mínimo R$ 5000,00.</p>
-                    <h3>Crédito</h3>
-                    <p>Com o plano ouro você tem direito a pedir R$ 5000,00 de crédito!</p>
-                    <h3>Empréstimo</h3>
-                    <p>Com o plano ouro você pode pedir até R$ 5000,00 de emprestimo, com juros de 20%.</p>
-                    <h3>Saldo Especial</h3>
-                    <p>Com o plano ouro você pode ficar até com R$ -5000,00.</p>
+                    <div className={planStyle.mainArticleContent}>
+                        <h3>Requisito</h3>
+                        <p>Para ser um membro do plano de ouro você precisa ter no mínimo R$ 5000,00.</p>
+                        <h3>Crédito</h3>
+                        <p>Com o plano ouro você tem direito a pedir R$ 5000,00 de crédito!</p>
+                        <h3>Empréstimo</h3>
+                        <p>Com o plano ouro você pode pedir até R$ 5000,00 de emprestimo, com juros de 20%.</p>
+                        <h3>Saldo Especial</h3>
+                        <p>Com o plano ouro você pode ficar até com R$ -5000,00.</p>
+                    </div>
                     <button>Selecionar</button>
                 </article>
 
-                <article>
+                <article className={`${planStyle.mainArticle} ${planStyle.platiun}`}>
                     <header>
                         <h2>Platina</h2>
                     </header>
-                    <h3>Requisito</h3>
-                    <p>Para ser um membro do plano de platina você precisa ter no mínimo R$ 15000,00.</p>
-                    <h3>Crédito</h3>
-                    <p>Com o plano platina você tem direito a pedir R$ 15000,00 de crédito!</p>
-                    <h3>Empréstimo</h3>
-                    <p>Com o plano platina você pode pedir até R$ 15000,00 de emprestimo, com juros de 20%.</p>
-                    <h3>Saldo Especial</h3>
-                    <p>Com o plano platina você pode ficar até com R$ -15000,00.</p>
-                    <h3>Financiamento</h3>
-                    <p>Com o plano platina você pode fazer financiamento de casas e carros!</p>
-                    <button>Selecionar</button>
+                    <div className={planStyle.mainArticleContent}>
+                        <h3>Requisito</h3>
+                        <p>Para ser um membro do plano de platina você precisa ter no mínimo R$ 15000,00.</p>
+                        <h3>Crédito</h3>
+                        <p>Com o plano platina você tem direito a pedir R$ 15000,00 de crédito!</p>
+                        <h3>Empréstimo</h3>
+                        <p>Com o plano platina você pode pedir até R$ 15000,00 de emprestimo, com juros de 20%.</p>
+                        <h3>Saldo Especial</h3>
+                        <p>Com o plano platina você pode ficar até com R$ -15000,00.</p>
+                        <h3>Financiamento</h3>
+                        <p>Com o plano platina você pode fazer financiamento de casas e carros!</p>
+                    </div>
+                    <button className={planStyle.lastButton}>Selecionar</button>
                 </article>
-                
+
             </section>
         </main>
     );
