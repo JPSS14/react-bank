b9a5cbddee24ff7c161d239a0f196f6aaa25e7db
JPSS14 <jp_scarabelli@hotmail.com>
2021-05-06 17:44:13 -0300

    react-bank UPDATE 55: Added Balance to Credito page and responsive

--- src/pages/credito.tsx
+++ src/pages/credito.tsx
@@ -1,17 +1,22 @@
 import style from '../styles/main.module.scss';
 import creditStyle from '../styles/Credit.module.scss';
+import { Balance } from '../components/Balance';
 
-export default function credito(){
-    return(
+export default function credito() {
+    return (
         <main className={style.main}>
+            <header className={style.mainFull90}>
+                <Balance />
+            </header>
             <div className={creditStyle.credit}>
                 <header>
                     <h1>Crédito Disponível</h1>
                 </header>
-                <div className={creditStyle.disponibleBalance}>
-                    <input type="text" value="R$ 1000,00" readOnly></input>
-                    <button type="button">Solicitar Crédito</button>
-                </div>
+
+                <p className={creditStyle.balance}>R$ 1000</p>
+                <p>Parabéns você pertence ao plano … e pode solicitar essa quantia de crédito, solicite agora e tenha a opção de compra com o seu crédito!</p>
+                <button type="button">Solicitar Crédito</button>
+
             </div>
         </main>
     );
